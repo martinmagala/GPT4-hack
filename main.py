44d94b03f270f71e76651744275a17fcdd3f33b1
yahyasamet <73041526+yahyasamet@users.noreply.github.com>
2024-02-18 01:36:31 +0100

    quiz generator

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from trulens_eval import TruChain, Feedback, OpenAI, Huggingface, Tru
+import streamlit as st
+from helpers.openai_utils import get_quiz_data
+from helpers.quiz_utils import string_to_list, get_randomized_options
+from helpers.toast_messages import get_random_toast
 
 hugs = Huggingface()
 openai = OpenAI()
@@ -13,7 +17,12 @@ tru = Tru()
 # Load environment variables from .streamlit/secrets.toml
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
-
+st.set_page_config(
+        page_title="Mediterranean AI",
+        page_icon="🧠",
+        layout="centered",
+        initial_sidebar_state="collapsed"
+    )
 
 # Define your page functions
 def home_page():
@@ -45,17 +54,7 @@ def Genarate_story():
     llm = ChatOpenAI(model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
 
-     # Build LLM chain
-    template2 = """Generate quiz from this story:
-            
-            Human: {human_input}
-            Chatbot:"""
-    prompt2 = PromptTemplate(
-        input_variables=["human_input"], template=template2
-    )
-    
-    llm2 = ChatOpenAI(model_name="gpt-3.5-turbo")
-    chain2 = LLMChain(llm=llm2, prompt=prompt2, verbose=True)
+
 
     # Question/answer relevance between overall question and answer.
     f_relevance = Feedback(openai.relevance).on_input_output()
@@ -70,9 +69,7 @@ def Genarate_story():
     chain_recorder = TruChain(
         chain, app_id="GPT4-story", feedbacks=[f_relevance, f_hate, f_violent, f_selfharm, f_maliciousness]
     )
-    chain_recorder2 = TruChain(
-        chain2, app_id="GPT4-Quiz", feedbacks=[f_relevance, f_hate, f_violent, f_selfharm, f_maliciousness]
-    )
+
 
     
     if "messages" not in st.session_state:
@@ -97,27 +94,79 @@ def Genarate_story():
             st.session_state.text =full_response
         st.session_state.messages.append(
             {"role": "assistant", "content": full_response})
-    if st.button("test") :
-
-        with st.chat_message("assistant"):
-                with chain_recorder2 as recording:
-                    prompt2=st.session_state.text
-                    full_response2 = chain2.run(prompt2)
-                message_placeholder = st.empty()
-                message_placeholder.markdown(full_response2 + "▌")
-                message_placeholder.markdown(full_response2)
-        st.session_state.messages.append(
-                {"role": "assistant", "content": full_response2})
         
     
         
 def Quiz_page():
     st.title("Test Your Comprehension")
-    
-    
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+    with st.form("user_input"):
+        if 'text' not in st.session_state:
+            st.session_state.text = ""
+        story_text =st.session_state.text
+        st.write("You entered:", st.session_state.text)
+        submitted = st.form_submit_button("Craft my quiz!")
+
+    if submitted or ('quiz_data_list' in st.session_state):
+
+        if not OPENAI_API_KEY:
+            st.info("Please fill out the OpenAI API Key to proceed. If you don't have one, you can obtain it [here](https://platform.openai.com/account/api-keys).")
+            st.stop()
+            
+        with st.spinner("Crafting your quiz...🤓"):
+            if submitted:
+
+                quiz_data_str = get_quiz_data(story_text, OPENAI_API_KEY)
+                st.session_state.quiz_data_list = string_to_list(quiz_data_str)
+
+                if 'user_answers' not in st.session_state:
+                    st.session_state.user_answers = [None for _ in st.session_state.quiz_data_list]
+                if 'correct_answers' not in st.session_state:
+                    st.session_state.correct_answers = []
+                if 'randomized_options' not in st.session_state:
+                    st.session_state.randomized_options = []
+
+                for q in st.session_state.quiz_data_list:
+                    options, correct_answer = get_randomized_options(q[1:])
+                    st.session_state.randomized_options.append(options)
+                    st.session_state.correct_answers.append(correct_answer)
+
+            with st.form(key='quiz_form'):
+                st.subheader("🧠 Quiz Time: Test Your Knowledge!", anchor=False)
+                for i, q in enumerate(st.session_state.quiz_data_list):
+                    options = st.session_state.randomized_options[i]
+                    default_index = st.session_state.user_answers[i] if st.session_state.user_answers[i] is not None else 0
+                    response = st.radio(q[0], options, index=default_index)
+                    user_choice_index = options.index(response)
+                    st.session_state.user_answers[i] = user_choice_index  # Update the stored answer right after fetching it
+
+
+                results_submitted = st.form_submit_button(label='Unveil My Score!')
+
+                if results_submitted:
+                    score = sum([ua == st.session_state.randomized_options[i].index(ca) for i, (ua, ca) in enumerate(zip(st.session_state.user_answers, st.session_state.correct_answers))])
+                    st.success(f"Your score: {score}/{len(st.session_state.quiz_data_list)}")
+
+                    if score == len(st.session_state.quiz_data_list):  # Check if all answers are correct
+                        st.balloons()
+                    else:
+                        incorrect_count = len(st.session_state.quiz_data_list) - score
+                        if incorrect_count == 1:
+                            st.warning(f"Almost perfect! You got 1 question wrong. Let's review it:")
+                        else:
+                            st.warning(f"Almost there! You got {incorrect_count} questions wrong. Let's review them:")
+
+                    for i, (ua, ca, q, ro) in enumerate(zip(st.session_state.user_answers, st.session_state.correct_answers, st.session_state.quiz_data_list, st.session_state.randomized_options)):
+                        with st.expander(f"Question {i + 1}", expanded=False):
+                            if ro[ua] != ca:
+                                st.info(f"Question: {q[0]}")
+                                st.error(f"Your answer: {ro[ua]}")
+                                st.success(f"Correct answer: {ca}")
+        
+        
 def contact_page():
-    st.title("Contact Page")
-    st.write("Feel free to contact us.")
+        st.title("Contact Page")
+        st.write("Feel free to contact us.")
     
 
 # Create a dictionary to map page names to page functions
