05f7752a10655055128ca8b481b33eaeca17c0ed
yahyasamet <73041526+yahyasamet@users.noreply.github.com>
2024-02-18 16:29:57 +0100

    modif

--- main.py
+++ main.py
@@ -108,12 +108,6 @@ def Genarate_story():
         st.markdown(html_content, unsafe_allow_html=True)
 
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-
-
 
 
 
